1ea7ddeaf917170523f755fa4cfd7f027aea1544
dhakal79 <n.dhakal@un-ihe.org>
2022-04-25 22:19:49 +0000

    add urls.py in service app

--- service/views.py
+++ service/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Service
 
 # Create your views here.
+def all_products(request):
+    """  
+    A view to show all products in the
+    database, including sorting and search results
+    """ 
+
+    services = Service.objects.all()
+
+    context = {
+        'services': services,
+    }
+
+    return render(request, 'service/service.html', context)
+
+
+    
