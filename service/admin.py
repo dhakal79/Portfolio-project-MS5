5188e6552ee83817c1e9cb07fefa213fc069bd8a
dhakal79 <n.dhakal@un-ihe.org>
2022-04-25 22:02:11 +0000

    customzed admin

--- service/admin.py
+++ service/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Service, Category
 
 # Register your models here.
+class ServiceAdmin(admin.ModelAdmin):
+    """Admin model for services."""
+    list_display = (
+        "name",
+        "blurb",
+        "genre",
+        "price",
+        "image",
+    )
+    ordering = ("name",)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """Admin model for Category"""
+    list_display = (
+        "friendly_name",
+        "name",
+    )
+
+
 admin.site.register(Service)
 admin.site.register(Category)
