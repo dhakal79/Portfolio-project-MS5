905d47a99761b6856b25cfa3fd1d1b2d3bd4783d
dhakal79 <n.dhakal@un-ihe.org>
2022-05-02 22:47:23 +0000

    generate secret key and debug

--- consultancy/settings.py
+++ consultancy/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oby3@-e7gb#5#eb&2oooh-e+gn$i-6#_yv3(09*$4_kze9v0iz'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['dhakalconsultancy.herokuapp.com', 'localhost']
 
