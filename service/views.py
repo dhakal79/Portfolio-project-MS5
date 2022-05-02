811f0e72dc0d3980edfef6c82654e33a4912dd10
dhakal79 <n.dhakal@un-ihe.org>
2022-05-02 07:27:31 +0000

    add category filtering

--- service/views.py
+++ service/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Service
+from .models import Service, Category
 
 # Create your views here.
 def all_products(request):
@@ -9,8 +9,14 @@ def all_products(request):
 
     services = Service.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            services = services.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -23,6 +29,8 @@ def all_products(request):
 
     context = {
         'services': services,
+        'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'service/service.html', context)
