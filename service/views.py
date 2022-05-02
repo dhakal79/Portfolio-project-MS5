d28d3e3451604e18fdfc2b5ce3273e2474009451
dhakal79 <n.dhakal@un-ihe.org>
2022-05-02 06:42:18 +0000

    add search functionality to the services

--- service/views.py
+++ service/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Service
 
 # Create your views here.
@@ -6,6 +8,18 @@ def all_products(request):
     """A view to show all products including sorting and search results"""
 
     services = Service.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('services'))
+            
+            queries = Q(name__icontains=query) | Q(blurb__icontains=query)
+            services = services.filter(queries)
+
 
     context = {
         'services': services,
