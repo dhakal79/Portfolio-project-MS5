2a45ac60d2b45f4466cdd8a173ec9a9d842b8c5a
dhakal79 <n.dhakal@un-ihe.org>
2022-05-05 21:52:41 +0000

    added add purchase functionality

--- purchase/urls.py
+++ purchase/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_purchase, name='view_purchase')
+    path('', views.view_purchase, name='view_purchase'),
+    path('add/<item_id>/', views.add_to_purchase, name='add_to_purchase'),
 ]
--- purchase/views.py
+++ purchase/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,4 +6,20 @@ def view_purchase(request):
     """ A view that renders the purchase contents page """
 
     return render(request, 'purchase/purchase.html')
+
+def add_to_purchase(request, item_id):
+    """ Add a quantity of the specified service to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    purchase = request.session.get('purchase', {})
+
+    if item_id in list(purchase.keys()):
+        purchase[item_id] += quantity
+    else:
+        purchase[item_id] = quantity
+
+    request.session['purchase'] = purchase
+    print(request.session['purchase'])
+    return redirect(redirect_url)
     
