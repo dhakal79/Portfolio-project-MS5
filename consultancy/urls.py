c3ab62451e05ee3dfc4846c1730b13e3c93b6fbf
dhakal79 <n.dhakal@un-ihe.org>
2022-05-06 11:25:19 +0000

    add app about to display the description of company

--- consultancy/urls.py
+++ consultancy/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('', include('home.urls')),
     path('service/', include('service.urls')),
     path('purchase/', include('purchase.urls')),
+    path('about/', include('about.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
