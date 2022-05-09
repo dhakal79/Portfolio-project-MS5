0d2d7b62a23118a044e8be47a6466b8be6c0972e
dhakal79 <n.dhakal@un-ihe.org>
2022-05-09 11:16:58 +0000

    check views.py in home

--- consultancy/settings.py
+++ consultancy/settings.py
@@ -13,9 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 if os.path.exists("env.py"):
-  import env 
+    import env 
 import dj_database_url
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
