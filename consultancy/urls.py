c6c5e120703c160802b3447fe523eb12840e46f1
dhakal79 <n.dhakal@un-ihe.org>
2022-05-09 11:39:25 +0000

    add message for subscribing and unscribing the newsletter

--- consultancy/urls.py
+++ consultancy/urls.py
@@ -27,3 +27,5 @@ urlpatterns = [
     path('about/', include('about.urls')),
     path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'home.views.error_404'
