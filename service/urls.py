6b655e820bde17862923363d82d9a1eb8dad38a6
dhakal79 <n.dhakal@un-ihe.org>
2022-05-18 21:26:08 +0000

    add addproduct, edit product templates in service

--- service/urls.py
+++ service/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='service'),
-    path('<service_id>', views.product_detail, name='product_detail'),      
+    path('<service_id>', views.product_detail, name='product_detail'),  
+    path('add/', views.add_product, name='add_product'),  
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
-
