9060ab12047ff07f5d599478a5906659b7aa68c1
dhakal79 <n.dhakal@un-ihe.org>
2022-05-20 23:17:27 +0000

    add views.py for checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+import json
+import stripe
+from django.shortcuts import (
+    render, redirect, reverse, get_object_or_404, HttpResponse)
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
-
+from purchase.contexts import purchase_contents
+from service.models import Service
 from .forms import OrderForm
 from .models import Order, OrderLineItem
-
-from service.models import Service
-from purchase.contexts import purchase_contents
-
-import stripe
-import json
 
 
 @require_POST
@@ -19,14 +17,14 @@ def cache_checkout_data(request):
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            'purchase': json.dumps(request.session.get('purchase', {})),
+            'bag': json.dumps(request.session.get('bag', {})),
             'save_info': request.POST.get('save_info'),
             'username': request.user,
         })
         return HttpResponse(status=200)
     except Exception as e:
         messages.error(request, 'Sorry, your payment cannot be \
-            processed right now. Please try again later.')
+            processed right now. Please try again.')
         return HttpResponse(content=e, status=400)
 
 
@@ -44,17 +42,16 @@ def checkout(request):
             'country': request.POST['country'],
             'postcode': request.POST['postcode'],
             'town_or_city': request.POST['town_or_city'],
-            'street_address1': request.POST['street_address1'],
-            'street_address2': request.POST['street_address2'],
+            'street_address_1': request.POST['street_address_1'],
+            'street_address_2': request.POST['street_address_2'],
             'county': request.POST['county'],
         }
-
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
             pid = request.POST.get('client_secret').split('_secret')[0]
             order.stripe_pid = pid
-            order.original_purchase = json.dumps(purchase)
+            order.original_purhcase = json.dumps(purchase)
             order.save()
             for item_id, item_data in purchase.items():
                 try:
@@ -67,33 +64,34 @@ def checkout(request):
                         )
                         order_line_item.save()
                     else:
-                        for size, quantity in item_data['items_by_size'].items():
+                        for weight, quantity in item_data['items_by_weight'].items():
                             order_line_item = OrderLineItem(
                                 order=order,
                                 service=service,
                                 quantity=quantity,
-                                product_size=size,
+                                product_weight=weight,
                             )
                             order_line_item.save()
                 except Service.DoesNotExist:
                     messages.error(request, (
-                        "One of the products in your purchase wasn't found in our database. "
+                        "One of the products in your bag wasn't \
+                            found in our database."
                         "Please call us for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('view_purchase'))
 
-            # Save the info to the user's profile if all is well
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
         else:
             messages.error(request, 'There was an error with your form. \
-                Please double check your information.')
+                Please double check your details.')
     else:
         purchase = request.session.get('purchase', {})
         if not purchase:
-            messages.error(request, "There's nothing in your bag at the moment")
-            return redirect(reverse('products'))
+            messages.error(request, "There's nothing in your bag at \
+                the moment")
+            return redirect(reverse('service'))
 
         current_purchase = purchase_contents(request)
         total = current_purchase['grand_total']
@@ -104,10 +102,27 @@ def checkout(request):
             currency=settings.STRIPE_CURRENCY,
         )
 
-        
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(initial={
+                    'full_name': profile.user.get_full_name(),
+                    'email': profile.user.email,
+                    'phone_number': profile.default_phone_number,
+                    'country': profile.default_country,
+                    'postcode': profile.default_postcode,
+                    'town_or_city': profile.default_town_or_city,
+                    'street_address_1': profile.default_street_address_1,
+                    'street_address_2': profile.default_street_address_2,
+                    'county': profile.default_county,
+                })
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
+
     if not stripe_public_key:
-        messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
+        messages.warning(request, 'Stripe public key is missing.')
 
     template = 'checkout/checkout.html'
     context = {
@@ -120,14 +135,33 @@ def checkout(request):
 
 
 def checkout_success(request, order_number):
-    """
-    Handle successful checkouts
-    """
+    """ Handles successful checkouts """
+
+    save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    messages.success(request, f'Order successfully processed! \
-        Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        order.user_profile = profile
+        order.save()
+
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_street_address_1': order.street_address_1,
+                'default_street_address_2': order.street_address_2,
+                'default_town_or_city': order.town_or_city,
+                'default_postcode': order.postcode,
+                'default_county': order.county,
+                'default_country': order.country,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+    messages.success(request, f'Order successfully processed. \
+        Your order number is {order_number}. A confirmation email \
+            will be sent to {order.email} shortly.')
 
     if 'purchase' in request.session:
         del request.session['purchase']
