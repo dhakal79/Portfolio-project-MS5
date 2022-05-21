71c0c6a61431d0a1275673245e02a9e7a87f3e9e
dhakal79 <n.dhakal@un-ihe.org>
2022-05-21 21:08:14 +0000

    add user profile url in setting.py

--- consultancy/urls.py
+++ consultancy/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('service/', include('service.urls')),
     path('purchase/', include('purchase.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
     path('about/', include('about.urls')),
     path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
