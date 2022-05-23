f1df4dcc819d2891143d68aec4f0be23a46d547a
dhakal79 <n.dhakal@un-ihe.org>
2022-05-23 23:45:22 +0000

    debug stripe payment

--- checkout/views.py
+++ checkout/views.py
@@ -19,7 +19,7 @@ def cache_checkout_data(request):
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
         stripe.PaymentIntent.modify(pid, metadata={
-            'bag': json.dumps(request.session.get('bag', {})),
+            'purchase': json.dumps(request.session.get('purchase', {})),
             'save_info': request.POST.get('save_info'),
             'username': request.user,
         })
@@ -44,8 +44,8 @@ def checkout(request):
             'country': request.POST['country'],
             'postcode': request.POST['postcode'],
             'town_or_city': request.POST['town_or_city'],
-            'street_address_1': request.POST['street_address_1'],
-            'street_address_2': request.POST['street_address_2'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
             'county': request.POST['county'],
         }
         order_form = OrderForm(form_data)
@@ -114,8 +114,8 @@ def checkout(request):
                     'country': profile.default_country,
                     'postcode': profile.default_postcode,
                     'town_or_city': profile.default_town_or_city,
-                    'street_address_1': profile.default_street_address_1,
-                    'street_address_2': profile.default_street_address_2,
+                    'street_address1': profile.default_street_address_1,
+                    'street_address2': profile.default_street_address_2,
                     'county': profile.default_county,
                 })
             except UserProfile.DoesNotExist:
@@ -150,8 +150,8 @@ def checkout_success(request, order_number):
         if save_info:
             profile_data = {
                 'default_phone_number': order.phone_number,
-                'default_street_address_1': order.street_address_1,
-                'default_street_address_2': order.street_address_2,
+                'default_street_address1': order.street_address_1,
+                'default_street_address2': order.street_address_2,
                 'default_town_or_city': order.town_or_city,
                 'default_postcode': order.postcode,
                 'default_county': order.county,
