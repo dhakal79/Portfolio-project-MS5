822d4eef2edd183d689aec2f31722eced406f653
dhakal79 <n.dhakal@un-ihe.org>
2022-05-25 19:52:29 +0000

    error page 404

--- consultancy/urls.py
+++ consultancy/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,8 +28,7 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('about/', include('about.urls')),
     path('contact/', include('contact.urls')),
-    path('publish/', include('publish.urls')),
-        
+    path('publish/', include('publish.urls')),        
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = "consultancy.views.page_not_found_view"
