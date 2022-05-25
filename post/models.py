9e8096a09d600a38bea99a2d9a1868eb627c56fd
dhakal79 <n.dhakal@un-ihe.org>
2022-05-25 09:19:20 +0000

    debug in model.py post

--- post/models.py
+++ post/models.py
@@ -4,35 +4,33 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
-STATUS = ((0, "Draft"), (1, "Published"))
-
 class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(User, on_delete=models.CASCADE,
-                               related_name="news_posts")
+                               related_name="news_post")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["-created_on"]
+        ordering = ["-publish" ,]
 
     def __str__(self):
         return self.title + ' |' + str(self.author)
 
     def get_absolute_url(self):
-        return reverse('news_posts')
+        return reverse('news_post')
 
 
     def get_absolute_url(self):
         return reverse("news_post",
-                       args=[self.publish.year, self.publish.month])
+                       args=[self.publish.year, self.publish.month,self.publish.day])
 
     def get_edit_url(self):
         return reverse("edit_post",
-                       args=[self.publish.year, self.publish.month])
+                       args=[self.publish.year, self.publish.month,self.publish.day])
 
     def get_delete_url(self):
         return reverse("delete_post",
-                       args=[self.publish.year, self.publish.month])
+                       args=[self.publish.year, self.publish.month,self.publish.day])
