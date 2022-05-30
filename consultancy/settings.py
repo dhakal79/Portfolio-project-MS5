0582f3b84aa6ac5f1db945c23215024bcb670c8c
dhakal79 <n.dhakal@un-ihe.org>
2022-05-30 09:23:12 +0000

    chnage account email verification to none in setting.py

--- consultancy/settings.py
+++ consultancy/settings.py
@@ -114,11 +114,12 @@ SITE_ID = 1
 # Django allauth custom config
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
+
 
 WSGI_APPLICATION = 'consultancy.wsgi.application'
 
